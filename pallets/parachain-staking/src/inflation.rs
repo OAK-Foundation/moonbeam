170d0ebad49fd8d36c7835cec72ab4fea62fe78b
Nikhil Vengal <ngvengal@gmail.com>
2022-05-26 13:41:29 -0700

    Use saturating_add

--- pallets/parachain-staking/src/inflation.rs
+++ pallets/parachain-staking/src/inflation.rs
@@ -22,6 +22,7 @@ use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
 use scale_info::TypeInfo;
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
+use sp_runtime::traits::Saturating;
 use sp_runtime::PerThing;
 use sp_runtime::{Perbill, RuntimeDebug};
 use substrate_fixed::transcendental::pow as floatpow;
@@ -92,7 +93,8 @@ pub fn annual_to_round<T: Config>(annual: Range<Perbill>) -> Range<Perbill> {
 
 /// Compute round issuance range from round inflation range and current total issuance
 pub fn round_issuance_range<T: Config>(round: Range<Perbill>) -> Range<BalanceOf<T>> {
-	let circulating = T::Currency::total_issuance() + T::AdditionalIssuance::additional_issuance();
+	let circulating =
+		T::Currency::total_issuance().saturating_add(T::AdditionalIssuance::additional_issuance());
 	Range {
 		min: round.min * circulating,
 		ideal: round.ideal * circulating,
